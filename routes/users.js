aa550cbd8d861549d2f8b4ddfa7ea7b15c91bf55
kishank11 <kishankanojia11@gmail.com>
2023-03-19 09:59:17 +0530

    changed fix

--- routes/users.js
+++ routes/users.js
@@ -7,19 +7,19 @@ const router = express.Router();
 
 router.route('/doc').post(addDoc)
 router.route('/getDocs').get(getDocs)
-router.route('/getDocById').get(getDocById)
+router.route('/getDocById/:id').get(getDocById)
 
 
 router.route('/hospital').post(addHospital)
 router.route('/getHospitals').get(getHospitals)
-router.route('/getHospitalById').get(getHospitalsById)
+router.route('/getHospitalById/:id').get(getHospitalsById)
 
 router.route('/relative').post(addRelative)
 router.route('/getRelatives').get(getRelatives)
-router.route('/getRelativeById').get(getRelativeById)
+router.route('/getRelativeById/:id').get(getRelativeById)
 
 router.route('/patient').post(addPatient)
 router.route('/getPatients').get(getPatients)
-router.route('/getPatientById').get(getPatientById)
+router.route('/getPatientById/:id').get(getPatientById)
 
 module.exports = router;
